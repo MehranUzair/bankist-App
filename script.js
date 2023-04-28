9206c98dbace7754151f5d76e58b0d88056f6f40
Mehran Uzair <contact.mehranuzair@gmail.com>
2023-04-28 11:15:23 +0500

    lecture code removed

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 'use strict';
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
 // BANKIST APP
 
 // Data
@@ -60,28 +57,3 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
-
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// console.log(arr.slice(-3));
-// console.log(arr.slice(2, -3));
-
-// console.log(arr.splice(2, 5));
-
-// console.log(arr.reverse());
-
-console.log(arr.join('='));
